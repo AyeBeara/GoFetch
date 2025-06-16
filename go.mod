bd75073f1755f0bb6bb67baa3e933ef5e3226191
Aidan G <adngatenbee03@gnmail.com>
2025-06-16 12:23:45 -0400

    building out sysfetch

--- go.mod
+++ go.mod
@@ -5,12 +5,24 @@ go 1.24.3
 require github.com/shirou/gopsutil/v4 v4.25.5
 
 require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.1.0 // indirect
+	github.com/containerd/console v1.0.5 // indirect
 	github.com/ebitengine/purego v0.8.4 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/pterm/pterm v0.12.81 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,19 +3,75 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
+	"os/user"
+	"runtime"
+	"strings"
 	"time"
 
+	"github.com/pterm/pterm"
 	"github.com/shirou/gopsutil/v4/cpu"
 )
 
 func main() {
-	sec, _ := time.ParseDuration("1s")
 
-	cpuUsage, err := cpu.Percent(sec, false)
-
+	cpuUsage, err := cpu.Percent(time.Second, false)
 	if err != nil {
 		log.Fatalf("Error getting CPU usage: %v", err)
 	}
 
-	fmt.Printf("CPU Usage: %v%%", cpuUsage[0])
+	user, _ := user.Current()
+
+	cpu_info, err := cpu.Info()
+	if err != nil {
+		log.Fatalf("Error getting CPU info: %v", err)
+	}
+
+	var ver []byte
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/C", "ver")
+		ver, err = cmd.Output()
+		if err != nil {
+			log.Fatalf("Error getting OS version: %v", err)
+		}
+	} else if runtime.GOOS == "linux" {
+		cmd := exec.Command("uname", "-rv")
+		ver, err = cmd.Output()
+		if err != nil {
+			log.Fatalf("Error getting OS version: %v", err)
+		}
+	} else {
+		log.Fatalf("Unsupported OS: %s. Please submit a pull request at github.com/AyeBeara/GoFetch", runtime.GOOS)
+	}
+
+	os_version := strings.TrimSpace(string(ver))
+
+	resources := []pterm.BulletListItem{
+		{
+			Level:     0,
+			Text:      fmt.Sprintf("User: %s", user.Username),
+			TextStyle: pterm.NewStyle(pterm.FgLightBlue),
+			Bullet:    "👤  ",
+		},
+		{
+			Level:     0,
+			Text:      fmt.Sprintf("OS: %s", os_version),
+			TextStyle: pterm.NewStyle(pterm.FgCyan),
+			Bullet:    "🖥️   ",
+		},
+		{
+			Level:     0,
+			Text:      fmt.Sprintf("CPU Model: %s", cpu_info[0].ModelName),
+			TextStyle: pterm.NewStyle(pterm.FgYellow),
+			Bullet:    "⚙️   ",
+		},
+		{
+			Level:     0,
+			Text:      fmt.Sprintf("CPU Usage: %0.2f%%", cpuUsage[0]),
+			TextStyle: pterm.NewStyle(pterm.FgGreen),
+			Bullet:    "📈  ",
+		},
+	}
+
+	pterm.DefaultBulletList.WithItems(resources).Render()
 }
